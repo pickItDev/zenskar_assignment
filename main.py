b54c66bbe60a07384cd0549f196310ce765155e7
Tanul <singhchandel2411@gmail.com>
2023-10-31 22:41:43 +0530

    -- Added print syntax for displaying data of customer bytes being added -- Added GET endpoint for getting customer data -- Added JWT Authentication

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 from flask import Flask, request, jsonify
 from collections import defaultdict
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'zenskar'  # Change this to a secure secret key.
+jwt = JWTManager(app)
+
+# Dummy user database for authentication
+users = {
+    "user1": "password1",
+    "user2": "password2"
+}
 
 # In-memory storage for demo purpose. Ideally, this should be a DB instance initiation
 monthly_usage = defaultdict(lambda: defaultdict(int))
 
+# JWT Authentication
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username in users and users[username] == password:
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"error": "Invalid credentials"}), 401
+
 # Routing requests to /usage
 @app.route('/usage', methods=['POST'])
-
+@jwt_required()
 def record_usage():
     data = request.get_json()
 
@@ -26,7 +49,19 @@ def record_usage():
     # Update the usage data for the customer in the corresponding month.
     monthly_usage[customer_id][f"{current_year}-{current_month:02}"] += bytes_sent
 
+    print(f"Customer {customer_id}: Total bytes sent = {bytes_sent} bytes")
     return jsonify({"message": "Usage recorded"}), 201
+
+# Get user month wise details using customer ID
+@app.route('/getUsageByCustomer', methods=['GET'])
+@jwt_required()
+def get_usage_by_customer():
+    customer_id = request.args.get('customer')
+
+    if customer_id in monthly_usage:
+        return jsonify({"customer": customer_id, "usage_data": monthly_usage[customer_id]})
+    else:
+        return jsonify({"error": "Data not found for the specified customer"}), 404
 
 
 if __name__ == '__main__':
